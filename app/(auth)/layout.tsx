3f409fca9031a3d1749a9d36f81f71bab38ca569
abdullahmia <abdullah149161@bsdi-bd.org>
2023-12-28 21:09:20 +0600

    Add authentication check in Layout component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,17 @@
+import { Session, getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../lib/authOptions";
 export interface LayoutProps {
   children: React.ReactNode;
 }
 
-export default function Layout(props: LayoutProps) {
+export default async function Layout(props: LayoutProps) {
+  const session: Session | null = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <main className="h-full flex justify-center items-center bg-fill-secondary">
       <div className="w-[500px] shadow rounded p-10 space-y-7 bg-white">
